45e3756468b9124eaf992ad6df7dfb7cb679b5d1
Waffle <74199677+WaffleDragon86@users.noreply.github.com>
2022-05-31 12:40:13 -0400

    Update sortArray.js

--- sortArray.js
+++ sortArray.js
@@ -12,7 +12,7 @@ function sort(arr) {
     for (let j=i-1;j>-1;j--) {
       if (arr[j + 1]<arr[j]) {
         [arr[j+1],arr[j]]=[arr[j],arr[j+1]];
-        //console.log(arr)
+        console.log(arr)
         times++
       }
 
